fb444190ff9586a220923da1dd417b94f53260cb
ketmar <ketmar@ketmar.no-ip.org>
2015-05-28 04:59:24 +0300

    new iv.hash.fasthash (and new hash API)

--- hash/fasthash.d
+++ hash/fasthash.d
@@ -28,86 +28,225 @@ module iv.hash.fasthash;
 
 
 /**
- * fasthash64 - 64-bit implementation of fasthash
+ * 64-bit implementation of fasthash
  *
  * Params:
  *   buf =  data buffer
  *   seed = the seed
  */
-ulong fasthash64(T) (const(T)[] buf, ulong seed=0) @trusted nothrow @nogc
-if (T.sizeof == 1)
-{
-  enum m = 0x880355f21e6d1965UL;
-  ulong h = seed^(buf.length*m);
+public struct FastHash {
+public:
+  enum has64bit = true;
+  enum has32bit = true;
 
-  ulong t;
-  foreach (immutable _; 0..buf.length/8) {
-    if (__ctfe) {
-      t =
-        cast(ulong)buf[0]|
-        (cast(ulong)buf[1]<<8)|
-        (cast(ulong)buf[2]<<16)|
-        (cast(ulong)buf[3]<<24)|
-        (cast(ulong)buf[4]<<32)|
-        (cast(ulong)buf[5]<<40)|
-        (cast(ulong)buf[6]<<48)|
-        (cast(ulong)buf[7]<<56);
-    } else {
-      t = *cast(ulong*)buf.ptr;
+private:
+  enum M = 0x880355f21e6d1965UL;
+
+private:
+  ulong hash; // current value; MUST BE FIRST
+  ulong accum; // we operate 64-bit chunks; high 3 bits of accum used as counter
+  ulong totallen;
+  ulong seed;
+
+public:
+@trusted:
+nothrow:
+@nogc:
+  /// construct state with seed
+  this (ulong aseed) { hash = seed = aseed; }
+
+  /// reset state
+  void reset () { accum = totallen = 0; hash = seed; }
+
+  /// reset state
+  void reset (ulong aseed) { accum = totallen = 0; hash = seed = aseed; }
+
+  /// process data block
+  void put(T) (scope const(T)[] data...) if (T.sizeof == 1) {
+    if (data.length == 0) return; // nothing to do
+    if (totallen == 0) hash = seed;
+    auto bytes = cast(const(ubyte)*)data.ptr;
+    auto len = data.length;
+    if (totallen+len < totallen) assert(0, "FastHash: too much data"); // overflow
+    totallen += len;
+    auto h = hash;
+    auto acc = accum;
+    // do we have something in accum?
+    if (acc) {
+      // consume any carry bytes
+      ubyte i = (acc>>61)&7;
+      assert(i != 0);
+      acc &= ~(7UL<<61);
+      while (i < 8 && len) {
+        acc |= (cast(ulong)(*bytes++))<<(i*8);
+        ++i;
+        --len;
+      }
+      if (i == 8) {
+        // got 8 bytes, process 'em
+        acc ^= acc>>23;
+        acc *= 0x2127599bf4325c37UL;
+        acc ^= acc>>47;
+        h ^= acc;
+        h *= M;
+        acc = 0;
+      } else {
+        // update counter
+        assert(i < 8);
+        assert(len == 0);
+        acc |= (cast(ulong)i)<<61;
+      }
     }
-    buf = buf[8..$];
-    t ^= t>>23;
-    t *= 0x2127599bf4325c37UL;
-    t ^= t>>47;
-    h ^= t;
-    h *= m;
-  }
-
-  t = 0;
-  switch (buf.length&7) {
-    case 7: t ^= cast(ulong)buf[6]<<48; goto case 6;
-    case 6: t ^= cast(ulong)buf[5]<<40; goto case 5;
-    case 5: t ^= cast(ulong)buf[4]<<32; goto case 4;
-    case 4: t ^= cast(ulong)buf[3]<<24; goto case 3;
-    case 3: t ^= cast(ulong)buf[2]<<16; goto case 2;
-    case 2: t ^= cast(ulong)buf[1]<<8; goto case 1;
-    case 1: t ^= cast(ulong)buf[0]; goto default;
-    default:
+    // now process 8-byte blocks
+    assert(len == 0 || acc == 0);
+    ulong t;
+    foreach (immutable _; 0..len/8) {
+      if (__ctfe) {
+        t =
+          cast(ulong)bytes[0]|
+          (cast(ulong)bytes[1]<<8)|
+          (cast(ulong)bytes[2]<<16)|
+          (cast(ulong)bytes[3]<<24)|
+          (cast(ulong)bytes[4]<<32)|
+          (cast(ulong)bytes[5]<<40)|
+          (cast(ulong)bytes[6]<<48)|
+          (cast(ulong)bytes[7]<<56);
+      } else {
+        version(LittleEndian) {
+          t = *cast(ulong*)bytes;
+        } else {
+          t =
+            cast(ulong)bytes[0]|
+            (cast(ulong)bytes[1]<<8)|
+            (cast(ulong)bytes[2]<<16)|
+            (cast(ulong)bytes[3]<<24)|
+            (cast(ulong)bytes[4]<<32)|
+            (cast(ulong)bytes[5]<<40)|
+            (cast(ulong)bytes[6]<<48)|
+            (cast(ulong)bytes[7]<<56);
+        }
+      }
+      bytes += 8;
       t ^= t>>23;
       t *= 0x2127599bf4325c37UL;
       t ^= t>>47;
       h ^= t;
-      h *= m;
-      break;
+      h *= M;
+    }
+    // do we have something to push into accum?
+    if ((len &= 7) != 0) {
+      foreach (immutable shift; 0..len) {
+        acc |= (cast(ulong)(*bytes++))<<(shift*8);
+      }
+      acc |= (cast(ulong)len)<<61;
+    }
+    hash = h;
+    accum = acc;
   }
 
-  h ^= h>>23;
-  h *= 0x2127599bf4325c37UL;
-  h ^= h>>47;
-  return h;
+  /// finalize a hash (i.e. return current result).
+  /// note that you can continue putting data, as this is not destructive
+  @property ulong result64 () const {
+    ulong h = hash;
+    if (totallen == 0) h = seed;
+    h ^= totallen*M;
+
+    ulong acc = accum;
+    if (acc) {
+      acc &= ~(7UL<<61);
+      h ^= acc&(7UL<<61);
+    }
+    acc ^= acc>>23;
+    acc *= 0x2127599bf4325c37UL;
+    acc ^= acc>>47;
+    h ^= acc;
+    h *= M;
+
+    h ^= h>>23;
+    h *= 0x2127599bf4325c37UL;
+    h ^= h>>47;
+    return h;
+  }
+
+  /// finalize a hash (i.e. return current result).
+  /// note that you can continue putting data, as this is not destructive
+  @property uint result32 () const {
+    auto h = result64;
+    // the following trick converts the 64-bit hashcode to Fermat
+    // residue, which shall retain information from both the higher
+    // and lower parts of hashcode.
+    return cast(uint)(h-(h>>32));
+  }
+
+  ulong finish64 () { auto res = result64; reset(); return res; }
+  ulong finish32 () { auto res = result32; reset(); return res; }
 }
 
-ulong fasthash64(T) (const(T)[] buf, uint seed=0) @trusted nothrow @nogc if (T.sizeof > 1) {
-  return fasthash64(cast(const(ubyte)[])buf, seed);
+
+/**
+ * 64-bit implementation of fasthash
+ *
+ * Params:
+ *   buf =  data buffer
+ *   seed = the seed
+ */
+ulong fastHash64(T) (const(T)[] buf, ulong seed=0) @trusted nothrow @nogc {
+  auto hh = FastHash(seed);
+  hh.put(buf);
+  return hh.result64;
 }
 
-
-uint fasthash32(T) (const(T)[] buf, uint seed=0) @trusted nothrow @nogc
-if (T.sizeof == 1)
-{
-  // the following trick converts the 64-bit hashcode to Fermat
-  // residue, which shall retain information from both the higher
-  // and lower parts of hashcode.
-  ulong h = fasthash64(buf, seed);
-  return cast(uint)(h-(h>>32));
-}
-
-ulong fasthash32(T) (const(T)[] buf, uint seed=0) @trusted nothrow @nogc if (T.sizeof > 1) {
-  return fasthash32(cast(const(ubyte)[])buf, seed);
+/**
+ * 32-bit implementation of fasthash
+ *
+ * Params:
+ *   buf =  data buffer
+ *   seed = the seed
+ */
+uint fastHash32(T) (const(T)[] buf, ulong seed=0) @trusted nothrow @nogc {
+  auto hh = FastHash(seed);
+  hh.put(buf);
+  return hh.result32;
 }
 
 
 unittest {
-  static assert(fasthash32("Alice & Miriel") == 0xed6586a5);
-  static assert(fasthash64("Alice & Miriel") == 0xa8ed28359652aedaUL);
+  static assert(fastHash32("Alice & Miriel") == 0x4773e2a3U);
+  static assert(fastHash64("Alice & Miriel") == 0xfa02b41e417696c1UL);
+
+  import std.traits : ReturnType;
+  import std.stdio;
+
+  void doTest(int sz) (string str, ulong hh) {
+    if (mixin("fastHash"~sz.stringof~"(str)") != hh) {
+      writeln("fastHash", sz, " fucked!");
+      assert(0);
+    }
+    foreach_reverse (immutable len; 0..str.length) {
+      {
+        auto hs = FastHash();
+        hs.put(str[0..len]);
+        foreach (immutable pos; len..str.length) hs.put(str[pos]);
+        auto res = mixin("hs.result"~sz.stringof);
+        if (res != hh) {
+          writeln("FastHash", sz, "(", len, ") fucked!");
+          assert(0);
+        }
+      }
+      {
+        auto h0 = mixin("fastHash"~sz.stringof~"(str[0..len])");
+        auto h1 = FastHash();
+        foreach (immutable pos; 0..len) h1.put(str[pos]);
+        if (mixin("h1.result"~sz.stringof) != h0) {
+          writeln("FastHashByOne", sz, "(", len, ") fucked!");
+          assert(0);
+        }
+      }
+    }
+  }
+
+  writefln("0x%08xU", fastHash32("Alice & Miriel"));
+  writefln("0x%016xUL", fastHash64("Alice & Miriel"));
+  doTest!32("Alice & Miriel", 0x4773e2a3U);
+  doTest!64("Alice & Miriel", 0xfa02b41e417696c1UL);
 }
