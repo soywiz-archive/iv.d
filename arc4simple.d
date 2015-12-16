4653b6163b740ff3aff990664ffc6edc5e3d848f
Ketmar Dark <ketmar@ketmar.no-ip.org>
2015-12-16 03:08:07 +0200

    hello, GPL

--- arc4simple.d
+++ arc4simple.d
@@ -1,41 +1,19 @@
-/* coded by Ketmar // Invisible Vector <ketmar@ketmar.no-ip.org>
+/* Invisible Vector Library
+ * coded by Ketmar // Invisible Vector <ketmar@ketmar.no-ip.org>
  * Understanding is not required. Only obedience.
  *
- *                   INVISIBLE VECTOR PUBLIC LICENSE
- *                       Version 1, September 2015
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Copyright (C) 2015 Ketmar Dark <ketmar@ketmar.no-ip.org>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * Everyone is permitted to copy and distribute verbatim or modified
- * copies of this license document, and changing it is allowed as long
- * as the name is changed.
- *
- *                   INVISIBLE VECTOR PUBLIC LICENSE
- *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
- *
- * 0. You may not use this software in either source or binary form, any
- *    software derived from this software, any library which uses either
- *    this software or code derived from this software in any other
- *    software on the territory of Russian Federation, either directly or
- *    indirectly via any chain of libraries.
- *
- * 1. Redistributions of this software in either source or binary form must
- *    retain this list of conditions and the following disclaimer.
- *
- * 2. Otherwise, you are allowed to use this software in any way that will
- *    not violate paragraphs 0 and 1 of this license.
- *
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * Authors: Ketmar // Invisible Vector <ketmar@ketmar.no-ip.org>
- * License: IVPLv1
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 module iv.arc4simple is aliced;
 
@@ -45,20 +23,18 @@ struct ARC4Codec {
   ubyte[256] m; // permutation table
   ubyte x, y; // permutation indicies
 
-@trusted:
-nothrow:
-@nogc:
+nothrow @trusted @nogc:
   this (const(void)[] key, usize skipBytes=4096) => reinit(key, skipBytes);
 
   void reinit (const(void)[] key, usize skipBytes=4096) {
     assert(key.length > 0);
-    auto keybytes = cast(const ubyte *)key.ptr;
+    auto keybytes = cast(const ubyte*)key.ptr;
     x = y = 0;
-    foreach (auto f; 0..256) m[f] = cast(ubyte)f;
+    foreach (immutable f; 0..256) m[f] = cast(ubyte)f;
     // create permutation table
     usize kidx = 0;
     ubyte c = 0;
-    foreach (auto f; 0..256) {
+    foreach (immutable f; 0..256) {
       auto kc = keybytes[kidx];
       auto a = m[f];
       c = (c+a+kc)&0xff;
@@ -78,7 +54,7 @@ nothrow:
 
   void processBuffer(T) (T[] buf) {
     usize len = T.sizeof*buf.length;
-    auto data = cast(ubyte *)buf.ptr;
+    auto data = cast(ubyte*)buf.ptr;
     foreach (; 0..len) {
       x = (x+1)&0xff;
       auto a = m[x];
--- dtw_test/gestures.d
+++ dtw_test/gestures.d
@@ -1,41 +1,19 @@
-/* coded by Ketmar // Invisible Vector <ketmar@ketmar.no-ip.org>
+/* Invisible Vector Library
+ * coded by Ketmar // Invisible Vector <ketmar@ketmar.no-ip.org>
  * Understanding is not required. Only obedience.
  *
- *                   INVISIBLE VECTOR PUBLIC LICENSE
- *                       Version 1, September 2015
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Copyright (C) 2015 Ketmar Dark <ketmar@ketmar.no-ip.org>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * Everyone is permitted to copy and distribute verbatim or modified
- * copies of this license document, and changing it is allowed as long
- * as the name is changed.
- *
- *                   INVISIBLE VECTOR PUBLIC LICENSE
- *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
- *
- * 0. You may not use this software in either source or binary form, any
- *    software derived from this software, any library which uses either
- *    this software or code derived from this software in any other
- *    software on the territory of Russian Federation, either directly or
- *    indirectly via any chain of libraries.
- *
- * 1. Redistributions of this software in either source or binary form must
- *    retain this list of conditions and the following disclaimer.
- *
- * 2. Otherwise, you are allowed to use this software in any way that will
- *    not violate paragraphs 0 and 1 of this license.
- *
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * Authors: Ketmar // Invisible Vector <ketmar@ketmar.no-ip.org>
- * License: IVPLv1
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 module gesturesdtw_test is aliced;
 private:
