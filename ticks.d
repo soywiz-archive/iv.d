09c7ee628c9551571883ad3f3919bc052bca4352
Ketmar Dark <ketmar@ketmar.no-ip.org>
2015-12-27 13:39:37 +0200

    iv.ticks: added untested windoze code

--- ticks.d
+++ ticks.d
@@ -28,56 +28,63 @@ extern(C) private int clock_gettime (clockid_t, timespec*) @trusted nothrow @nog
 version(linux) {
   private enum CLOCK_MONOTONIC_RAW = 4;
   private enum CLOCK_MONOTONIC_COARSE = 6;
-} else {
-  static assert(0, "sorry, only GNU/Linux for now; please, fix `getTicks()` and company for your OS!");
-}
 
-
-shared static this () {
-  initializeClock();
-}
-
-
-private __gshared timespec videolib_clock_stt;
-private __gshared int gtClockType = CLOCK_MONOTONIC_COARSE;
-
-
-nothrow @trusted @nogc:
-
-private void initializeClock () {
-  timespec cres = void;
-  bool inited = false;
-  if (clock_getres(gtClockType, &cres) == 0) {
-    if (cres.tv_sec == 0 && cres.tv_nsec <= cast(long)1000000*1 /*1 ms*/) inited = true;
+  shared static this () {
+    initializeClock();
   }
-  if (!inited) {
-    gtClockType = CLOCK_MONOTONIC_RAW;
+
+
+  private __gshared timespec videolib_clock_stt;
+  private __gshared int gtClockType = CLOCK_MONOTONIC_COARSE;
+
+
+  nothrow @trusted @nogc:
+
+  private void initializeClock () {
+    timespec cres = void;
+    bool inited = false;
     if (clock_getres(gtClockType, &cres) == 0) {
       if (cres.tv_sec == 0 && cres.tv_nsec <= cast(long)1000000*1 /*1 ms*/) inited = true;
     }
+    if (!inited) {
+      gtClockType = CLOCK_MONOTONIC_RAW;
+      if (clock_getres(gtClockType, &cres) == 0) {
+        if (cres.tv_sec == 0 && cres.tv_nsec <= cast(long)1000000*1 /*1 ms*/) inited = true;
+      }
+    }
+    if (!inited) assert(0, "FATAL: can't initialize clock subsystem!");
+    if (clock_gettime(gtClockType, &videolib_clock_stt) != 0) {
+      assert(0, "FATAL: can't initialize clock subsystem!");
+    }
   }
-  if (!inited) assert(0, "FATAL: can't initialize clock subsystem!");
-  if (clock_gettime(gtClockType, &videolib_clock_stt) != 0) {
-    assert(0, "FATAL: can't initialize clock subsystem!");
+
+
+  /** returns monitonically increasing time; starting value is UNDEFINED (i.e. can be any number); milliseconds */
+  ulong getTicks () {
+    static if (__VERSION__ > 2067) pragma(inline, true);
+    timespec ts = void;
+    if (clock_gettime(gtClockType, &ts) != 0) assert(0, "FATAL: can't get real-time clock value!\n");
+    // ah, ignore nanoseconds in videolib_clock_stt->stt here: we need only 'differential' time, and it can start with something weird
+    return (cast(ulong)(ts.tv_sec-videolib_clock_stt.tv_sec))*1000+ts.tv_nsec/1000000+1;
   }
-}
 
 
-/** returns monitonically increasing time; starting value is UNDEFINED (i.e. can be any number); milliseconds */
-ulong getTicks () {
-  static if (__VERSION__ > 2067) pragma(inline, true);
-  timespec ts = void;
-  if (clock_gettime(gtClockType, &ts) != 0) assert(0, "FATAL: can't get real-time clock value!\n");
-  // ah, ignore nanoseconds in videolib_clock_stt->stt here: we need only 'differential' time, and it can start with something weird
-  return (cast(ulong)(ts.tv_sec-videolib_clock_stt.tv_sec))*1000+ts.tv_nsec/1000000+1;
-}
+  /** returns monitonically increasing time; starting value is UNDEFINED (i.e. can be any number); microseconds */
+  ulong getTicksMicro () {
+    static if (__VERSION__ > 2067) pragma(inline, true);
+    timespec ts = void;
+    if (clock_gettime(gtClockType, &ts) != 0) assert(0, "FATAL: can't get real-time clock value!\n");
+    // ah, ignore nanoseconds in videolib_clock_stt->stt here: we need only 'differential' time, and it can start with something weird
+    return (cast(ulong)(ts.tv_sec-videolib_clock_stt.tv_sec))*1000000+ts.tv_nsec/1000+1;
+  }
+} else version(Windows) {
+  private import core.sys.windows.winbase : GetTickCount;
 
+  ulong getTicks () { return GetTickCount(); }
 
-/** returns monitonically increasing time; starting value is UNDEFINED (i.e. can be any number); microseconds */
-ulong getTicksMicro () {
-  static if (__VERSION__ > 2067) pragma(inline, true);
-  timespec ts = void;
-  if (clock_gettime(gtClockType, &ts) != 0) assert(0, "FATAL: can't get real-time clock value!\n");
-  // ah, ignore nanoseconds in videolib_clock_stt->stt here: we need only 'differential' time, and it can start with something weird
-  return (cast(ulong)(ts.tv_sec-videolib_clock_stt.tv_sec))*1000000+ts.tv_nsec/1000+1;
+  ulong getTicksMicro () {
+    static assert(0, "iv.ticks.getTicksMicro() is not implemented on windoze");
+  }
+} else {
+  static assert(0, "sorry, only GNU/Linux for now; please, fix `getTicks()` and company for your OS!");
 }
