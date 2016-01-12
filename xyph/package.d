6b65f1ec328e36d9a850ae60f2352ff129380200
Ketmar Dark <ketmar@ketmar.no-ip.org>
2016-01-12 19:07:31 +0200

    completely new libogg and libvorbis bindings

---
+++ xyph/package.d
@@ -0,0 +1,17 @@
+/********************************************************************
+ *                                                                  *
+ * THIS FILE IS PART OF THE OggVorbis SOFTWARE CODEC SOURCE CODE.   *
+ * USE, DISTRIBUTION AND REPRODUCTION OF THIS LIBRARY SOURCE IS     *
+ * GOVERNED BY A BSD-STYLE SOURCE LICENSE INCLUDED WITH THIS SOURCE *
+ * IN 'COPYING'. PLEASE READ THESE TERMS BEFORE DISTRIBUTING.       *
+ *                                                                  *
+ * THE OggVorbis SOURCE CODE IS (C) COPYRIGHT 1994-2007             *
+ * by the Xiph.Org Foundation http://www.xiph.org/                  *
+ *                                                                  *
+ ********************************************************************/
+module iv.xyph;
+
+public import iv.xyph.ogg;
+public import iv.xyph.vorbis;
+public import iv.xyph.vorbisenc;
+public import iv.xyph.vorbisfile;
