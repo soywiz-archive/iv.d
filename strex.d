9832994e42bf280bfb7f97e311a676895ffef0b6
Ketmar Dark <ketmar@ketmar.no-ip.org>
2016-05-13 08:38:24 +0300

    iv.strex: `detab()`, `byLine()`, `outdentAll()` (fuckin' autodecoding in std.string!)

--- strex.d
+++ strex.d
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-module iv.strex;
+module iv.strex is aliced;
 
 
 /// quote string: append double quotes, screen all special chars;
@@ -31,7 +31,7 @@ string quote (const(char)[] s) {
 }
 
 
-ptrdiff_t indexOf (const(void)[] hay, const(void)[] need, size_t stIdx=0) pure @trusted nothrow @nogc {
+sptrdiff indexOf (const(void)[] hay, const(void)[] need, usize stIdx=0) pure @trusted nothrow @nogc {
   if (hay.length <= stIdx || need.length == 0 ||
       need.length > hay.length-stIdx
   ) {
@@ -39,17 +39,17 @@ ptrdiff_t indexOf (const(void)[] hay, const(void)[] need, size_t stIdx=0) pure @
   } else {
     //import iv.strex : memmem;
     auto res = memmem(hay.ptr+stIdx, hay.length-stIdx, need.ptr, need.length);
-    return (res !is null ? cast(ptrdiff_t)(res-hay.ptr) : -1);
+    return (res !is null ? cast(sptrdiff)(res-hay.ptr) : -1);
   }
 }
 
 
-ptrdiff_t indexOf (const(void)[] hay, ubyte ch, size_t stIdx=0) pure @trusted nothrow @nogc {
+sptrdiff indexOf (const(void)[] hay, ubyte ch, usize stIdx=0) pure @trusted nothrow @nogc {
   return indexOf(hay, (&ch)[0..1], stIdx);
 }
 
 
-unittest {
+version(test_strex) unittest {
   assert(indexOf("Alice & Miriel", " & ") == 5);
   assert(indexOf("Alice & Miriel", " &!") == -1);
   assert(indexOf("Alice & Miriel", "Alice & Miriel was here!") == -1);
@@ -65,17 +65,162 @@ unittest {
   assert(indexOf("Alice & Miriel", "Miriel", 9) == -1);
 }
 
+
+string detab (const(char)[] s, uint tabSize=8) {
+  assert(tabSize > 0);
+
+  import std.array : appender;
+  auto res = appender!string();
+  uint col = 0;
+
+  foreach (char ch; s) {
+    if (ch == '\n' || ch == '\r') {
+      col = 0;
+    } else if (ch == '\t') {
+      auto spins = tabSize-col%tabSize;
+      col += spins;
+      while (spins-- > 1) res.put(' ');
+      ch = ' ';
+    } else {
+      ++col;
+    }
+    res.put(ch);
+  }
+
+  return res.data;
+}
+
+
+version(test_strex) unittest {
+  assert(detab(" \n\tx", 9) == " \n         x");
+  assert(detab("  ab\t asdf ") == "  ab     asdf ");
+}
+
+
+auto byLine(T) (T s) if (is(T : const(char)[])) {
+  static struct Range(T) {
+  nothrow @safe @nogc:
+  private:
+    T s;
+    usize llen, npos;
+    this (T as) { s = as; popFront(); }
+  public:
+    @property bool empty () const { pragma(inline, true); return (s.length == 0); }
+    @property T front () const { pragma(inline, true); return cast(T)s[0..llen]; } // fuckin' const!
+    auto save () const @trusted { Range!T res = void; res.s = s; res.llen = llen; res.npos = npos; return res; }
+    void popFront () @trusted {
+      s = s[npos..$];
+      llen = npos = 0;
+      while (npos < s.length) {
+        if (s.ptr[npos] == '\r') {
+          llen = npos;
+          if (s.length-npos > 1 && s.ptr[npos+1] == '\n') ++npos;
+          ++npos;
+          return;
+        }
+        if (s.ptr[npos] == '\n') {
+          llen = npos;
+          ++npos;
+          return;
+        }
+        ++npos;
+      }
+      llen = npos;
+    }
+  }
+  return Range!T(s);
+}
+
+/*
+version(test_strex) unittest {
+  enum s = q{
+       import std.stdio;
+       void main() {
+           writeln("Hello");
+       }
+    };
+    enum ugly = q{
+import std.stdio;
+void main() {
+    writeln("Hello");
+}
+};
+
+  foreach (auto line; s.byLine) {
+    import std.stdio;
+    writeln("LN: [", line, "]");
+  }
+
+  foreach (auto line; ugly.byLine) {
+    import std.stdio;
+    writeln("LN: [", line, "]");
+  }
+}
+*/
+
+// string should be detabbed!
+string outdentAll (const(char)[] s) {
+  import std.array : appender;
+  // first calculate maximum indent spaces
+  uint maxspc = uint.max;
+  foreach (auto line; s.byLine) {
+    uint col = 0;
+    while (col < line.length && line.ptr[col] <= ' ') {
+      if (line.ptr[col] == '\t') assert(0, "can't outdent shit with tabs");
+      ++col;
+    }
+    if (col >= line.length) continue; // empty line, don't care
+    if (col < maxspc) maxspc = col;
+    if (col == 0) break; // nothing to do anymore
+  }
+
+  auto res = appender!string();
+  foreach (auto line; s.byLine) {
+    uint col = 0;
+    while (col < line.length && line.ptr[col] <= ' ') ++col;
+    if (col < line.length) {
+      // non-empty line
+      res.put(line[maxspc..$]);
+    }
+    res.put('\n');
+  }
+
+  return res.data;
+}
+
+
+version(test_strex) unittest {
+    enum pretty = q{
+       import std.stdio;
+       void main() {
+           writeln("Hello");
+       }
+    }.outdentAll;
+
+    enum ugly = q{
+import std.stdio;
+void main() {
+    writeln("Hello");
+}
+
+};
+
+  import std.stdio;
+  assert(pretty == ugly);
+}
+
+
 @system:
 nothrow:
 @nogc:
 pure:
 version(linux) {
-  extern(C) inout(void)* memmem (inout(void)* haystack, size_t haystacklen, inout(void)* needle, size_t needlelen);
+  extern(C) inout(void)* memmem (inout(void)* haystack, usize haystacklen, inout(void)* needle, usize needlelen);
 } else {
-  inout(void)* memmem (inout(void)* haystack, size_t haystacklen, inout(void)* needle, size_t needlelen) {
+  inout(void)* memmem (inout(void)* haystack, usize haystacklen, inout(void)* needle, usize needlelen) {
     auto h = cast(const(ubyte)*)haystack;
     auto n = cast(const(ubyte)*)needle;
-    // size_t is unsigned
+    // usize is unsigned
     if (needlelen > haystacklen) return null;
     foreach (immutable i; 0..haystacklen-needlelen+1) {
       import core.stdc.string : memcmp;
