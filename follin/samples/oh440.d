3a7c63203fc053e3cf974e288cdac02e34c67d22
Ketmar Dark <ketmar@ketmar.no-ip.org>
2016-06-19 01:45:45 +0300

    follin: added 440Hz generator sample

---
+++ follin/samples/oh440.d
@@ -0,0 +1,78 @@
+/* Invisible Vector Library
+ * coded by Ketmar // Invisible Vector <ketmar@ketmar.no-ip.org>
+ * Understanding is not required. Only obedience.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+module oh440;
+
+import iv.follin;
+
+
+// ////////////////////////////////////////////////////////////////////////// //
+public class Oh440Hertz : TflChannel {
+  enum Freq = 440;
+  //enum Rate = 48000;
+
+  int smpnum;
+
+  this() () {
+    //sampleRate = Rate;
+    stereo = false;
+    volume = 128;
+  }
+
+  override uint fillFrames (float[] buf) nothrow @nogc {
+    foreach (ref f; buf) {
+      import std.math : sin, PI;
+      float t = cast(float)smpnum/sampleRate;
+      t = sin(Freq*2*PI*t);
+      f = t;
+      //f *= (1-t); // fade effect
+      ++smpnum;
+    }
+    return buf.length; // return number of frames
+  }
+}
+
+
+// ////////////////////////////////////////////////////////////////////////// //
+void playSfx () {
+  import core.sys.posix.unistd : usleep;
+
+  auto chan = new Oh440Hertz();
+  chan.volume = 128;
+  if (!tflAddChannel("sfx", chan, TFLdefault, TflChannel.QualitySfx)) {
+    import core.stdc.stdio;
+    fprintf(stderr, "ERROR: can't add sfx channel!\n");
+    return;
+  }
+  //tflPaused = false; // play it!
+  foreach (immutable _; 0..1000/10*3) {
+    if (!tflIsChannelAlive("sfx")) break;
+    usleep(10*1000); // 10 ms
+  }
+  //tflPaused = true; // just in case
+}
+
+
+// ////////////////////////////////////////////////////////////////////////// //
+void main (string[] args) {
+  import std.random;
+
+  tflInit();
+  scope(exit) tflDeinit();
+
+  playSfx();
+}
