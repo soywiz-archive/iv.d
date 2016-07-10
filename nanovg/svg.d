679d89228a1622996b73a5a3fe8e636ea1263f55
Ketmar Dark <ketmar@ketmar.no-ip.org>
2016-07-10 12:34:05 +0300

    made nanosvg "less restrictive" -- as i got some feedback on that (for once, lol)

--- nanovg/svg.d
+++ nanovg/svg.d
@@ -23,25 +23,8 @@
  * Arc calculation code based on canvg (https://code.google.com/p/canvg/)
  *
  * Bounding box calculation based on http://blog.hackers-cafe.net/2009/06/how-to-calculate-bezier-curves-bounding.html
- */
-/* Invisible Vector Library
+ *
  * ported by Ketmar // Invisible Vector <ketmar@ketmar.no-ip.org>
- * Understanding is not required. Only obedience.
- * yes, this D port is GPLed. thanks to all "active" members of D
- * community, and for all (zero) feedback posts.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 module iv.nanovg.svg;
 
