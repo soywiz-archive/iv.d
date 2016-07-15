f44d55cedd20966d245bdd1238b27ea7d10e1cf1
Ketmar Dark <ketmar@ketmar.no-ip.org>
2016-07-15 12:47:59 +0300

    more cosmetix

--- nanovg/backgl.d
+++ nanovg/backgl.d
@@ -710,101 +710,105 @@ bool glnvg__renderCreate (void* uptr) {
   GLNVGcontext* gl = cast(GLNVGcontext*)uptr;
   enum align_ = 4;
 
-  enum shaderHeader =
-    "#define UNIFORMARRAY_SIZE 11\n"~
-    "\n";
+  enum shaderHeader = "#define UNIFORMARRAY_SIZE 11\n";
 
-  enum fillVertShader =
-    " uniform vec2 viewSize;\n"~
-    " attribute vec2 vertex;\n"~
-    " attribute vec2 tcoord;\n"~
-    " varying vec2 ftcoord;\n"~
-    " varying vec2 fpos;\n"~
-    "void main(void) {\n"~
-    " ftcoord = tcoord;\n"~
-    " fpos = vertex;\n"~
-    " gl_Position = vec4(2.0*vertex.x/viewSize.x-1.0, 1.0-2.0*vertex.y/viewSize.y, 0, 1);\n"~
-    "}\n";
+  enum fillVertShader = q{
+    uniform vec2 viewSize;
+    attribute vec2 vertex;
+    attribute vec2 tcoord;
+    varying vec2 ftcoord;
+    varying vec2 fpos;
+    void main (void) {
+      ftcoord = tcoord;
+      fpos = vertex;
+      gl_Position = vec4(2.0*vertex.x/viewSize.x-1.0, 1.0-2.0*vertex.y/viewSize.y, 0, 1);
+    }
+  };
 
-  enum fillFragShader =
-    " uniform vec4 frag[UNIFORMARRAY_SIZE];\n"~
-    " uniform sampler2D tex;\n"~
-    " varying vec2 ftcoord;\n"~
-    " varying vec2 fpos;\n"~
-    " #define scissorMat mat3(frag[0].xyz, frag[1].xyz, frag[2].xyz)\n"~
-    " #define paintMat mat3(frag[3].xyz, frag[4].xyz, frag[5].xyz)\n"~
-    " #define innerCol frag[6]\n"~
-    " #define outerCol frag[7]\n"~
-    " #define scissorExt frag[8].xy\n"~
-    " #define scissorScale frag[8].zw\n"~
-    " #define extent frag[9].xy\n"~
-    " #define radius frag[9].z\n"~
-    " #define feather frag[9].w\n"~
-    " #define strokeMult frag[10].x\n"~
-    " #define strokeThr frag[10].y\n"~
-    " #define texType int(frag[10].z)\n"~
-    " #define type int(frag[10].w)\n"~
-    "\n"~
-    "float sdroundrect(vec2 pt, vec2 ext, float rad) {\n"~
-    " vec2 ext2 = ext-vec2(rad,rad);\n"~
-    " vec2 d = abs(pt)-ext2;\n"~
-    " return min(max(d.x,d.y),0.0)+length(max(d,0.0))-rad;\n"~
-    "}\n"~
-    "\n"~
-    "// Scissoring\n"~
-    "float scissorMask(vec2 p) {\n"~
-    " vec2 sc = (abs((scissorMat*vec3(p,1.0)).xy)-scissorExt);\n"~
-    " sc = vec2(0.5,0.5)-sc*scissorScale;\n"~
-    " return clamp(sc.x,0.0,1.0)*clamp(sc.y,0.0,1.0);\n"~
-    "}\n"~
-    "#ifdef EDGE_AA\n"~
-    "// Stroke - from [0..1] to clipped pyramid, where the slope is 1px.\n"~
-    "float strokeMask() {\n"~
-    " return min(1.0, (1.0-abs(ftcoord.x*2.0-1.0))*strokeMult)*min(1.0, ftcoord.y);\n"~
-    "}\n"~
-    "#endif\n"~
-    "\n"~
-    "void main(void) {\n"~
-    "   vec4 result;\n"~
-    " float scissor = scissorMask(fpos);\n"~
-    "#ifdef EDGE_AA\n"~
-    " float strokeAlpha = strokeMask();\n"~
-    "#else\n"~
-    " float strokeAlpha = 1.0;\n"~
-    "#endif\n"~
-    " if (type == 0) {      // Gradient\n"~
-    "   // Calculate gradient color using box gradient\n"~
-    "   vec2 pt = (paintMat*vec3(fpos,1.0)).xy;\n"~
-    "   float d = clamp((sdroundrect(pt, extent, radius)+feather*0.5)/feather, 0.0, 1.0);\n"~
-    "   vec4 color = mix(innerCol,outerCol,d);\n"~
-    "   // Combine alpha\n"~
-    "   color *= strokeAlpha*scissor;\n"~
-    "   result = color;\n"~
-    " } else if (type == 1) {   // Image\n"~
-    "   // Calculate color fron texture\n"~
-    "   vec2 pt = (paintMat*vec3(fpos,1.0)).xy/extent;\n"~
-    "   vec4 color = texture2D(tex, pt);\n"~
-    "   if (texType == 1) color = vec4(color.xyz*color.w,color.w);\n"~
-    "   if (texType == 2) color = vec4(color.x);\n"~
-    "   // Apply color tint and alpha.\n"~
-    "   color *= innerCol;\n"~
-    "   // Combine alpha\n"~
-    "   color *= strokeAlpha*scissor;\n"~
-    "   result = color;\n"~
-    " } else if (type == 2) {   // Stencil fill\n"~
-    "   result = vec4(1,1,1,1);\n"~
-    " } else if (type == 3) {   // Textured tris\n"~
-    "   vec4 color = texture2D(tex, ftcoord);\n"~
-    "   if (texType == 1) color = vec4(color.xyz*color.w,color.w);\n"~
-    "   if (texType == 2) color = vec4(color.x);\n"~
-    "   color *= scissor;\n"~
-    "   result = color*innerCol;\n"~
-    " }\n"~
-    "#ifdef EDGE_AA\n"~
-    " if (strokeAlpha < strokeThr) discard;\n"~
-    "#endif\n"~
-    " gl_FragColor = result;\n"~
-    "}\n";
+  enum fillFragShader = q{
+    uniform vec4 frag[UNIFORMARRAY_SIZE];
+    uniform sampler2D tex;
+    varying vec2 ftcoord;
+    varying vec2 fpos;
+    #define scissorMat mat3(frag[0].xyz, frag[1].xyz, frag[2].xyz)
+    #define paintMat mat3(frag[3].xyz, frag[4].xyz, frag[5].xyz)
+    #define innerCol frag[6]
+    #define outerCol frag[7]
+    #define scissorExt frag[8].xy
+    #define scissorScale frag[8].zw
+    #define extent frag[9].xy
+    #define radius frag[9].z
+    #define feather frag[9].w
+    #define strokeMult frag[10].x
+    #define strokeThr frag[10].y
+    #define texType int(frag[10].z)
+    #define type int(frag[10].w)
+
+    float sdroundrect (vec2 pt, vec2 ext, float rad) {
+      vec2 ext2 = ext-vec2(rad, rad);
+      vec2 d = abs(pt)-ext2;
+      return min(max(d.x, d.y), 0.0)+length(max(d, 0.0))-rad;
+    }
+
+    // Scissoring
+    float scissorMask (vec2 p) {
+      vec2 sc = (abs((scissorMat*vec3(p, 1.0)).xy)-scissorExt);
+      sc = vec2(0.5, 0.5)-sc*scissorScale;
+      return clamp(sc.x, 0.0, 1.0)*clamp(sc.y, 0.0, 1.0);
+    }
+    #ifdef EDGE_AA
+    // Stroke - from [0..1] to clipped pyramid, where the slope is 1px.
+    float strokeMask () {
+      return min(1.0, (1.0-abs(ftcoord.x*2.0-1.0))*strokeMult)*min(1.0, ftcoord.y);
+    }
+    #endif
+
+    void main(void) {
+      vec4 result;
+      float scissor = scissorMask(fpos);
+      #ifdef EDGE_AA
+      float strokeAlpha = strokeMask();
+      #else
+      float strokeAlpha = 1.0;
+      #endif
+      if (type == 0) {
+        // Gradient
+        // Calculate gradient color using box gradient
+        vec2 pt = (paintMat*vec3(fpos, 1.0)).xy;
+        float d = clamp((sdroundrect(pt, extent, radius)+feather*0.5)/feather, 0.0, 1.0);
+        vec4 color = mix(innerCol, outerCol, d);
+        // Combine alpha
+        color *= strokeAlpha*scissor;
+        result = color;
+      } else if (type == 1) {
+        // Image
+        // Calculate color from texture
+        vec2 pt = (paintMat*vec3(fpos, 1.0)).xy/extent;
+        vec4 color = texture2D(tex, pt);
+        if (texType == 1) color = vec4(color.xyz*color.w, color.w);
+        if (texType == 2) color = vec4(color.x);
+        // Apply color tint and alpha.
+        color *= innerCol;
+        // Combine alpha
+        color *= strokeAlpha*scissor;
+        result = color;
+      } else if (type == 2) {
+        // Stencil fill
+        result = vec4(1, 1, 1, 1);
+      } else if (type == 3) {
+        // Textured tris
+        vec4 color = texture2D(tex, ftcoord);
+        if (texType == 1) color = vec4(color.xyz*color.w, color.w);
+        if (texType == 2) color = vec4(color.x);
+        color *= scissor;
+        result = color*innerCol;
+      }
+      #ifdef EDGE_AA
+      if (strokeAlpha < strokeThr) discard;
+      #endif
+      gl_FragColor = result;
+    }
+  };
 
   glnvg__checkError(gl, "init");
 
