29fb5ffe94564eb3f89de8239b9cdd910a47958b
Ketmar Dark <ketmar@ketmar.no-ip.org>
2016-08-06 22:27:32 +0300

    added keystone bindings

---
+++ keystone/test00.d
@@ -0,0 +1,33 @@
+import iv.keystone;
+import std.stdio;
+
+
+// separate assembly instructions by ; or \n
+immutable string CODE = "INC ecx; DEC edx";
+
+void main () {
+  ks_engine *ks;
+  ks_err err;
+  size_t count;
+  ubyte* encode;
+  size_t size;
+
+  err = ks_open(KS_ARCH_X86, KS_MODE_32, &ks);
+  if (err != KS_ERR_OK) assert(0, "FATAL: can't init keystone");
+
+  if (ks_asm(ks, CODE.ptr, 0, &encode, &size, &count) != KS_ERR_OK) {
+    import std.string : fromStringz;
+    writefln("ERROR: ks_asm() failed & count = %s, error = %s (%s)", count, ks_errno(ks), ks_strerror(ks_errno(ks)).fromStringz);
+  } else {
+    writefln("%s = ", CODE);
+    foreach (immutable i; 0..size) writef("%02x ", encode[i]);
+    writeln();
+    writefln("Compiled: %s bytes, statements: %s", size, count);
+  }
+
+  // NOTE: free encode after usage to avoid leaking memory
+  ks_free(encode);
+
+  // close Keystone instance when done
+  ks_close(ks);
+}
