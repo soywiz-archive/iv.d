7cd8be5ab7ec67f28468025c651f63196c87be7e
Ketmar Dark <ketmar@ketmar.no-ip.org>
2016-09-16 17:19:42 +0300

    iv.weakref: more fixes, valinized

--- weakref.d
+++ weakref.d
@@ -17,7 +17,7 @@
  */
 // a thread-safe weak reference implementation
 // http://forum.dlang.org/thread/jjote0$1cql$1@digitalmars.com
-module iv.weakref is aliced;
+module iv.weakref;
 
 import core.atomic, core.memory;
 
@@ -34,16 +34,16 @@ final class Weak(T : Object) {
   // gets such a GC, we should push strongly for built-in weak
   // references.
 
-  private usize mObject;
-  private usize mPtr;
-  private uhash mHash;
+  private size_t mObject;
+  private size_t mPtr;
+  private size_t mHash;
 
   this (T obj=null) @trusted {
     hook(obj);
   }
 
   @property T object () const @trusted nothrow {
-    auto obj = cast(T)cast(void*)atomicLoad(*cast(shared)&mObject);
+    auto obj = cast(T)cast(void*)(atomicLoad(*cast(shared)&mObject)^0xa5a5a5a5u);
     // we've moved obj into the GC-scanned stack space, so it's now
     // safe to ask the GC whether the object is still alive.
     // note that even if the cast and assignment of the obj local
@@ -54,7 +54,7 @@ final class Weak(T : Object) {
   }
 
   @property void object (T obj) @trusted {
-    auto oobj = cast(T)cast(void*)atomicLoad(*cast(shared)&mObject);
+    auto oobj = cast(T)cast(void*)(atomicLoad(*cast(shared)&mObject)^0xa5a5a5a5u);
     if (oobj !is null && GC.addrOf(cast(void*)oobj)) unhook(oobj);
     oobj = null;
     hook(obj);
@@ -64,24 +64,24 @@ final class Weak(T : Object) {
     return (object is null);
   }
 
-  void clear () @trusted => object = null;
+  void clear () @trusted { object = null; }
 
-  void opAssign (T obj) @trusted => object = obj;
+  void opAssign (T obj) @trusted { object = obj; }
 
   private void hook (Object obj) @trusted {
     if (obj !is null) {
-      //auto ptr = cast(usize)cast(void*)obj;
+      //auto ptr = cast(size_t)cast(void*)obj;
       // fix from Andrej Mitrovic
-      auto ptr = cast(usize)*(cast(void**)&obj);
+      auto ptr = cast(size_t)*(cast(void**)&obj);
       // we use atomics because not all architectures may guarantee atomic store and load of these values
-      atomicStore(*cast(shared)&mObject, ptr);
+      atomicStore(*cast(shared)&mObject, ptr^0xa5a5a5a5u);
       // only assigned once, so no atomics
-      mPtr = ptr;
+      mPtr = ptr^0xa5a5a5a5u;
       mHash = typeid(T).getHash(&obj);
       rt_attachDisposeEvent(obj, &unhook);
       GC.setAttr(cast(void*)this, GC.BlkAttr.NO_SCAN);
     } else {
-      atomicStore(*cast(shared)&mObject, cast(usize)0);
+      atomicStore(*cast(shared)&mObject, cast(size_t)0^0xa5a5a5a5u);
     }
   }
 
@@ -91,21 +91,21 @@ final class Weak(T : Object) {
     // if we don't null mObject when it is collected, the check
     // in object could return false positives where the GC has
     // reused the memory for a new object.
-    atomicStore(*cast(shared)&mObject, cast(usize)0);
+    atomicStore(*cast(shared)&mObject, cast(size_t)0^0xa5a5a5a5u);
   }
 
-  override equals_t opEquals (Object o) @trusted nothrow {
+  override bool opEquals (Object o) @trusted nothrow {
     if (this is o) return true;
-    if (auto weak = cast(Weak!T)o) return mPtr == weak.mPtr;
+    if (auto weak = cast(Weak!T)o) return (mPtr == weak.mPtr);
     return false;
   }
 
   override int opCmp (Object o) @trusted nothrow {
-    if (auto weak = cast(Weak!T)o) return (mPtr > weak.mPtr);
+    if (auto weak = cast(Weak!T)o) return (mPtr > weak.mPtr ? 1 : mPtr < weak.mPtr ? -1 : 0);
     return 1;
   }
 
-  override uhash toHash () @trusted nothrow {
+  override size_t toHash () @trusted nothrow {
     auto obj = object;
     return (obj ? typeid(T).getHash(&obj) : mHash);
   }
@@ -123,28 +123,33 @@ version(weakref_test) unittest {
 
   static class A {
     int n;
-    this (int nn) @trusted nothrow => n = nn;
-    ~this () @trusted => writeln("A:~this()");
+    this (int nn) @trusted nothrow { n = nn; }
+    ~this () @trusted @nogc { import core.stdc.stdio : printf; printf("A:~this()\n"); }
   }
 
   auto a = new A(42);
   //auto wr = new Weak!A(a);
   Weak!A wr = new Weak!A(a);
+  writefln("w=%s", wr.empty);
+  assert(!wr.empty);
+  delete a;
+  writefln("w=%s", wr.empty);
   GC.collect();
   writefln("w=%s", wr.empty);
-  a = null;
-  writefln("w=%s", wr.empty);
-  GC.collect();
-  writefln("w=%s", wr.empty);
+  assert(wr.empty);
 
   a = new A(666);
   wr.object = a;
   writefln("w=%s", wr.empty);
+  assert(!wr.empty);
   wr.clear();
   writefln("w=%s", wr.empty);
+  assert(wr.empty);
 
   wr = a;
   writefln("w=%s", wr.empty);
+  assert(!wr.empty);
   wr.object = null;
   writefln("w=%s", wr.empty);
+  assert(wr.empty);
 }
