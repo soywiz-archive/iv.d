a156247fbd48cdfc13d01a99ae3f1778dfd940ff
Ketmar Dark <ketmar@ketmar.no-ip.org>
2016-12-26 07:21:27 +0200

    oops

--- libopus.d
+++ libopus.d
@@ -24,7 +24,7 @@
    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-module libopus;
+module iv.libopus;
 pragma(lib, "opus");
 
 extern(C) nothrow @nogc:
