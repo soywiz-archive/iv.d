4eb666709fe6e069130c8607a011e67fafee5f7f
Ketmar Dark <ketmar@ketmar.no-ip.org>
2017-01-07 01:29:33 +0200

    altereq code cleanup

--- trestex/mbeq_1197.d
+++ trestex/mbeq_1197.d
@@ -29,35 +29,21 @@ version(FFTW3) {
 
 // ////////////////////////////////////////////////////////////////////////// //
 public struct MBEQ {
-public:
+private:
   enum FFT_LENGTH = 1024;
   enum OVER_SAMP = 4;
+  enum STEP_SIZE = FFT_LENGTH/OVER_SAMP;
+
+public:
   version(mbeq_39) {
     enum BANDS = 39;
   } else {
     enum BANDS = 15;
   }
+  enum LATENCY = FFT_LENGTH-STEP_SIZE;
 
 public:
-  // [-70..30]
-  // 50Hz gain (low shelving)
-  // 100Hz gain (float value)
-  // 156Hz gain (float value)
-  // 220Hz gain (float value)
-  // 311Hz gain (float value)
-  // 440Hz gain (float value)
-  // 622Hz gain (float value)
-  // 880Hz gain (float value)
-  // 1250Hz gain (float value)
-  // 1750Hz gain (float value)
-  // 2500Hz gain (float value)
-  // 3500Hz gain (float value)
-  // 5000Hz gain (float value)
-  // 10000Hz gain (float value)
-  // 20000Hz gain (float value)
-  float[BANDS] bands = 0;
-  //float* input;
-  //float* output;
+  float[BANDS] bands = 0; // [-70..30)
   int latency;
   int* bin_base;
   float* bin_delta;
@@ -71,7 +57,7 @@ public:
   fft_plan plan_rc;
   fftw_real* realx;
   float* window;
-  int s_rate;
+  int smpRate;
 
   this (int asrate) {
     setup(asrate);
@@ -88,8 +74,8 @@ public:
     //scope(failure) cleanup();
 
     if (asrate < 1024 || asrate > 48000) assert(0, "invalid sampling rate");
-    s_rate = asrate;
-    float hz_per_bin = cast(float)s_rate/cast(float)FFT_LENGTH;
+    smpRate = asrate;
+    float hz_per_bin = cast(float)asrate/cast(float)FFT_LENGTH;
 
     zalloc(in_fifo, FFT_LENGTH);
     zalloc(out_fifo, FFT_LENGTH);
@@ -105,19 +91,6 @@ public:
       plan_rc = fftwf_plan_r2r_1d(FFT_LENGTH, realx, comp, FFTW_R2HC, FFTW_MEASURE);
       plan_cr = fftwf_plan_r2r_1d(FFT_LENGTH, comp, realx, FFTW_HC2R, FFTW_MEASURE);
     } else {
-      //plan_rc = rfftw_create_plan(FFT_LENGTH, FFTW_REAL_TO_COMPLEX, FFTW_ESTIMATE);
-      //plan_cr = rfftw_create_plan(FFT_LENGTH, FFTW_COMPLEX_TO_REAL, FFTW_ESTIMATE);
-      //data=new REALTYPE[nsamples];for (int i=0;i<nsamples;i++) data[i]=0.0;
-
-      //planfftw=fftwf_plan_r2r_1d(nsamples,data,data,FFTW_R2HC,FFTW_ESTIMATE);
-      //planifftw=fftwf_plan_r2r_1d(nsamples,data,data,FFTW_HC2R,FFTW_ESTIMATE);
-
-      //datar = new kiss_fft_scalar[nsamples+2];
-      //for (int i=0;i<nsamples+2;i++) datar[i]=0.0;
-      //datac=new kiss_fft_cpx[nsamples/2+2];
-      //for (int i=0;i<nsamples/2+2;i++) datac[i].r=datac[i].i=0.0;
-      //plankfft = kiss_fftr_alloc(nsamples,0,0,0);
-      //plankifft = kiss_fftr_alloc(nsamples,1,0,0);
       plan_rc = kiss_fftr_alloc(FFT_LENGTH, 0, null, null);
       plan_cr = kiss_fftr_alloc(FFT_LENGTH, 1, null, null);
     }
@@ -141,7 +114,7 @@ public:
       bin_base[bin] = 0;
       bin_delta[bin++] = 0.0f;
     }
-    for (int i = 1; i < BANDS-1 && bin < (FFT_LENGTH/2)-1 && bandfrqs[i+1] < s_rate/2; ++i) {
+    for (int i = 1; i < BANDS-1 && bin < (FFT_LENGTH/2)-1 && bandfrqs[i+1] < asrate/2; ++i) {
       float last_bin = bin;
       float next_bin = (bandfrqs[i+1])/hz_per_bin;
       while (bin <= next_bin) {
@@ -155,6 +128,8 @@ public:
       bin_base[bin] = BANDS-1;
       bin_delta[bin] = 0.0f;
     }
+
+    latency = LATENCY;
   }
 
   void cleanup () {
@@ -168,6 +143,7 @@ public:
     xfree(bin_delta);
     xfree(db_table);
     version(FFTW3) {
+      //??? no need to do FFTW cleanup?
     } else {
       kiss_fft_free(plan_rc);
       kiss_fft_free(plan_cr);
@@ -179,6 +155,8 @@ public:
   // input: input (array of floats of length sample_count)
   // output: output (array of floats of length sample_count)
   void run (fftw_real[] output, const(fftw_real)[] input, uint stride=1, uint ofs=0) {
+    import core.stdc.string : memmove;
+
     if (output.length < input.length) assert(0, "wtf?!");
     if (stride == 0) assert(0, "wtf?!");
     if (ofs >= input.length || input.length < stride) return;
@@ -188,9 +166,6 @@ public:
     gains[$-1] = 0.0f;
 
     float[FFT_LENGTH/2] coefs = void;
-
-    enum step_size = FFT_LENGTH/OVER_SAMP;
-    enum fft_latency = FFT_LENGTH-step_size;
 
     // convert gains from dB to co-efficents
     foreach (immutable i; 0..BANDS) {
@@ -205,20 +180,19 @@ public:
       coefs[bin] = ((1.0f-bin_delta[bin])*gains[bin_base[bin]])+(bin_delta[bin]*gains[bin_base[bin]+1]);
     }
 
-    if (fifo_pos == 0) fifo_pos = fft_latency;
+    if (fifo_pos == 0) fifo_pos = LATENCY;
 
     foreach (immutable pos; 0..input.length/stride) {
       in_fifo[fifo_pos] = input.ptr[pos*stride+ofs];
-      output.ptr[pos*stride+ofs] = out_fifo[fifo_pos-fft_latency];
+      output.ptr[pos*stride+ofs] = out_fifo[fifo_pos-LATENCY];
       ++fifo_pos;
-
       // if the FIFO is full
       if (fifo_pos >= FFT_LENGTH) {
-        fifo_pos = fft_latency;
+        fifo_pos = LATENCY;
         // window input FIFO
         foreach (immutable i; 0..FFT_LENGTH) realx[i] = in_fifo[i]*window[i];
-        // run the real->complex transform
         version(FFTW3) {
+          // run the real->complex transform
           fftwf_execute(plan_rc);
           // multiply the bins magnitudes by the coeficients
           comp[0] *= coefs[0];
@@ -226,7 +200,10 @@ public:
             comp[i] *= coefs[i];
             comp[FFT_LENGTH-i] *= coefs[i];
           }
+          // run the complex->real transform
+          fftwf_execute(plan_cr);
         } else {
+          // run the real->complex transform
           //rfftw_one(plan_rc, realx, comp);
           realx[FFT_LENGTH..FFT_LENGTH+16] = 0; // just in case
           comp[FFT_LENGTH-16..FFT_LENGTH+16] = 0; // just in case
@@ -237,42 +214,30 @@ public:
             comp[i*2+0] *= coefs[i];
             comp[i*2+1] *= coefs[i];
           }
-        }
-        //rfft(FFT_LENGTH, 1, realx);
-
-        // run the complex->real transform
-        version(FFTW3) {
-          fftwf_execute(plan_cr);
-        } else {
+          // run the complex->real transform
           //rfftw_one(plan_cr, comp, realx);
           kiss_fftri(plan_cr, cast(const(kiss_fft_cpx)*)comp, realx);
         }
         // window into the output accumulator
         foreach (immutable i; 0..FFT_LENGTH) out_accum[i] += 0.9186162f*window[i]*realx[i]/(FFT_LENGTH*OVER_SAMP);
-        foreach (immutable i; 0..step_size) out_fifo[i] = out_accum[i];
+        foreach (immutable i; 0..STEP_SIZE) out_fifo[i] = out_accum[i];
         // shift output accumulator
-        {
-          import core.stdc.string : memmove;
-          memmove(out_accum, out_accum+step_size, FFT_LENGTH*float.sizeof);
-        }
+        memmove(out_accum, out_accum+STEP_SIZE, FFT_LENGTH*float.sizeof);
         // shift input fifo
-        foreach (immutable i; 0..fft_latency) in_fifo[i] = in_fifo[i+step_size];
+        foreach (immutable i; 0..LATENCY) in_fifo[i] = in_fifo[i+STEP_SIZE];
       }
     }
-    // store the fifo_position
-    //plugin_data.fifo_pos = fifo_pos;
-    //*(plugin_data.latency) = fft_latency;
-    latency = fft_latency;
   }
 
+private:
   version(mbeq_39) {
-    private static immutable float[BANDS] bandfrqs = [
+    static immutable float[BANDS] bandfrqs = [
       12.5, 25, 37.5, 50, 62.5, 75, 87.5, 100, 125, 150, 175, 200, 250, 300, 350, 400,
       500, 600, 700, 800, 1000, 1200, 1400, 1600, 2000, 2400, 2800, 3200, 4000, 4800,
       5600, 6400, 8000, 9600, 11200, 12800, 16000, 19200, 22400
     ];
   } else {
-    private static immutable float[BANDS] bandfrqs = [
+    static immutable float[BANDS] bandfrqs = [
        50.00f,  100.00f,  155.56f,  220.00f,  311.13f, 440.00f, 622.25f,
       880.00f, 1244.51f, 1760.00f, 2489.02f, 3519.95, 4978.04f, 9956.08f,
       19912.16f
@@ -280,8 +245,8 @@ public:
   }
 
 private:
-static:
-  T* xalloc(T) (uint count=1) nothrow @trusted @nogc {
+  /*
+  static T* xalloc(T) (uint count=1) nothrow @trusted @nogc {
     import core.stdc.stdlib : malloc;
     import core.stdc.string : memcpy, memset;
     if (count == 0 || count > 1024*1024*8) assert(0, "wtf?!");
@@ -304,18 +269,9 @@ static:
     }
     return res;
   }
+  */
 
-  T* zalloc(T) (uint count=1) nothrow @trusted @nogc {
-    import core.stdc.stdlib : malloc;
-    import core.stdc.string : memcpy, memset;
-    if (count == 0 || count > 1024*1024*8) assert(0, "wtf?!");
-    auto res = cast(T*)malloc(T.sizeof*count);
-    if (res is null) assert(0, "out of memory");
-    memset(res, 0, T.sizeof*count);
-    return res;
-  }
-
-  void zalloc(T) (ref T* res, uint count=1) nothrow @trusted @nogc {
+  static void zalloc(T) (ref T* res, uint count) nothrow @trusted @nogc {
     import core.stdc.stdlib : malloc;
     import core.stdc.string : memcpy, memset;
     if (count == 0 || count > 1024*1024*8) assert(0, "wtf?!");
@@ -324,7 +280,7 @@ static:
     memset(res, 0, T.sizeof*count);
   }
 
-  void xfree(T) (ref T* ptr) nothrow @trusted @nogc {
+  static void xfree(T) (ref T* ptr) nothrow @trusted @nogc {
     import core.stdc.stdlib : free;
     if (ptr !is null) { free(ptr); ptr = null; }
   }
