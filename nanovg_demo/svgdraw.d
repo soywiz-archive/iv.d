f98f44ae2c339b4e8d5db759b800942648dcb9e7
Ketmar Dark <ketmar@ketmar.no-ip.org>
2017-03-03 02:01:58 +0200

    cosmetix

--- nanovg_demo/svgdraw.d
+++ nanovg_demo/svgdraw.d
@@ -36,28 +36,63 @@ void main (string[] args) {
   NVGContext vg = null;
   PerfGraph fps;
 
+  int minw = 32, minh = 32;
+  string fname;
+  for (size_t idx = 1; idx < args.length; ++idx) {
+    import std.conv : to;
+    string a = args[idx];
+    if (a == "-w" || a == "-h") {
+      char d = a[1];
+      a = a[2..$];
+      if (a.length == 0) {
+        ++idx;
+        if (idx >= args.length) assert(0, "out of args");
+        a = args[idx];
+      }
+      if (d == 'w') minw = to!int(a); else minh = to!int(a);
+    } else if (a == "--width") {
+      ++idx;
+      if (idx >= args.length) assert(0, "out of args");
+      minw = to!int(args[idx]);
+    } else if (a == "--height") {
+      ++idx;
+      if (idx >= args.length) assert(0, "out of args");
+      minh = to!int(args[idx]);
+    } else if (a == "--") {
+      ++idx;
+      if (idx >= args.length) assert(0, "out of args");
+      fname = args[idx];
+      break;
+    } else {
+      if (fname !is null) assert(0, "too many filenames");
+      fname = args[idx];
+    }
+  }
+  if (fname.length == 0) assert(0, "no filename");
+
   NSVG* svg;
   scope(exit) svg.kill();
   {
     import std.stdio : writeln;
     import core.time, std.datetime;
     auto stt = MonoTime.currTime;
-    svg = nsvgParseFromFile(args.length > 1 ? args[1] : "data/svg/tiger.svg");
+    svg = nsvgParseFromFile(fname);
     auto dur = (MonoTime.currTime-stt).total!"msecs";
     writeln("loading took ", dur, " milliseconds (", dur/1000.0, " seconds)");
     { import std.stdio; writeln(args.length > 1 ? args[1] : "data/svg/tiger.svg"); }
   }
 
-  //writefln("size: %f x %f", svg.width, svg.height);
+  printf("size: %f x %f\n", cast(double)svg.width, cast(double)svg.height);
   GWidth = cast(int)svg.width;
   GHeight = cast(int)svg.height;
-  if (GWidth < 32) GWidth = 32;
-  if (GHeight < 32) GHeight = 32;
+  if (GWidth < minw) GWidth = minw;
+  if (GHeight < minh) GHeight = minh;
 
   int vgimg;
 
 
   bool doQuit = false;
+  bool drawFPS = false;
 
   //setOpenGLContextVersion(3, 2); // up to GLSL 150
   setOpenGLContextVersion(2, 0); // it's enough
@@ -105,7 +140,7 @@ void main (string[] args) {
         vg.fillPaint(vg.imagePattern(0, 0, GWidth, GHeight, 0, vgimg, 1));
         vg.fill();
       }
-      if (fps !is null) fps.render(vg, 5, 5);
+      if (fps !is null && drawFPS) fps.render(vg, 5, 5);
       vg.endFrame();
     }
   };
@@ -122,7 +157,7 @@ void main (string[] args) {
       writeln("Could not init nanovg.");
       //sdwindow.close();
     }
-    enum FNN = "/home/ketmar/ttf/ms/arial.ttf";
+    enum FNN = "/home/ketmar/ttf/ms/verdana.ttf";
     vg.createFont("sans", FNN);
     {
       ubyte[] svgraster;
@@ -159,6 +194,7 @@ void main (string[] args) {
       if (!event.pressed) return;
       switch (event.key) {
         case Key.Escape: sdwindow.close(); break;
+        case Key.Space: drawFPS = !drawFPS; break;
         default:
       }
     },
