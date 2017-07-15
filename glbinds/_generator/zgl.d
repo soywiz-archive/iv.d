1bd14e1cd983292c596244c72aed48c55bcb54ee
Ketmar Dark <ketmar@ketmar.no-ip.org>
2017-07-15 07:24:07 +0300

    iv.glbinds: reworked generator; regenerated bindings

--- glbinds/_generator/zgl.d
+++ glbinds/_generator/zgl.d
@@ -7,74 +7,150 @@ import iv.vfs.io;
 
 Document gldom;
 
-auto xre = ctRegex!(`^const\s+([^\s*]+)\s*(\*+)\s*(.+)$`);
-auto repre = ctRegex!(`\b(ref|in|out)\b`);
-//auto fuckre = ctRegex!(`\*\s*const\s*\*`);
-auto fuckre = ctRegex!(`\bconst\s*(\*+)`);
 
+// ////////////////////////////////////////////////////////////////////////// //
+struct TypeAndName {
+  string name;
+  string type;
+  bool needReturn;
+
+  this (Element pp) {
+    assert(pp !is null);
+
+    string getName (Element nr) {
+      auto ne = nr.querySelector("name");
+      assert(ne !is null);
+      auto res = ne.innerText.xstrip;
+      assert(res.length);
+      return res;
+    }
+
+    string textNoNameIntr (Element el) {
+      string s;
+      foreach (Element child; el.children) {
+        if (child.tagName == "name") continue;
+        if (child.nodeType != NodeType.Text) s ~= textNoNameIntr(child); else s ~= child.nodeValue();
+      }
+      return s;
+    }
+
+    string fixTypes (string s) {
+      s = s.xstrip;
+
+      static struct Repl {
+        StaticRegex!char re;
+        string repl;
+        bool cont;
+      }
+
+      static Repl[$] replaces = [
+        Repl(ctRegex!(`^(Display|DMparams|XVisualInfo|GLXVideoCaptureDeviceNV)\s*\*$`), "/*$1*/void*"),
+        Repl(ctRegex!(`^(GLXFBConfigSGIX|GLXFBConfig|GLXContext)\s*\*$`), "/*$1*/void**"),
+        Repl(ctRegex!(`^(GLXFBConfigSGIX|GLXFBConfig|GLXContext)$`), "/*$1*/void*"),
+        // shitdoze start
+        Repl(ctRegex!(`^(HVIDEOINPUTDEVICENV|HVIDEOOUTPUTDEVICENV|HGPUNV|HPVIDEODEV)\s*\*$`), "/*$1*/void**"),
+        Repl(ctRegex!(`^(HPBUFFERARB|HPBUFFEREXT|HVIDEOOUTPUTDEVICENV|HPVIDEODEV|HPGPUNV|HGPUNV|HVIDEOINPUTDEVICENV|PGPU_DEVICE)$`), "/*$1*/void*"),
+        Repl(ctRegex!(`^const\s+(HGPUNV)\s*\*$`), "/*$1*/const(void)**"),
+        // shitdoze end
+        Repl(ctRegex!(`^const\s+(GLXContext)$`), "/*$1*/const(void)*"),
+        Repl(ctRegex!(`\bunsigned\s+int\b`), "uint", true),
+        Repl(ctRegex!(`\bunsigned\s+long\b`), "c_ulong", true),
+        Repl(ctRegex!(`\blong\b`), "c_long", true),
+        Repl(ctRegex!(`\bBool\b`), "int", true),
+        Repl(ctRegex!(`\bint32_t\b`), "int", true),
+        Repl(ctRegex!(`\bint64_t\b`), "long", true),
+        Repl(ctRegex!(`\bGLXVideoDeviceNV\b`), "uint", true), // unsigned int
+        Repl(ctRegex!(`^(Status|Font|GLXPbufferSGIX|Window|GLXDrawable|GLXVideoCaptureDeviceNV|GLXPbuffer|GLXPixmap|Pixmap|Colormap|GLXVideoSourceSGIX|GLXWindow|GLXVideoCaptureDeviceNV|GLXContextID)$`), "/*$1*/c_ulong"), // XID
+        //Repl(ctRegex!(`^(GLXVideoCaptureDeviceNV)$`), "/*$1*/c_ulong"), // XID
+        Repl(ctRegex!(`^(DMbuffer)$`), "/*$1*/void*"),
+        Repl(ctRegex!(`^const\s+([^\s*]+)\s*(\*+)$`), "const($1)$2"),
+      ];
+
+      //if (s.indexOf("Display") >= 0) stderr.writeln("|", s, "|");
+
+      foreach (ref repl; replaces[]) {
+        auto mt = s.matchFirst(repl.re);
+        if (!mt.empty) {
+          s = s.replaceAll(repl.re, repl.repl);
+          if (!repl.cont) break;
+        }
+      }
+      for (;;) {
+        auto pos = s.indexOf("* ");
+        if (pos < 0) break;
+        ++pos;
+        while (pos < s.length && s[pos] == ' ') s = s[0..pos]~s[pos+1..$];
+      }
+      for (;;) {
+        auto pos = s.indexOf(" *");
+        if (pos < 0) break;
+        while (pos > 0 && s[pos-1] == ' ') --pos;
+        while (pos < s.length && s[pos] == ' ') s = s[0..pos]~s[pos+1..$];
+      }
+      if (s == "const GLchar*const*") s = "const(GLchar)**";
+      if (s == "const void*const*") s = "const(void)**";
+      return s;
+    }
+
+    string textNoName (Element el) {
+      return fixTypes(textNoNameIntr(el));
+    }
+
+    name = getName(pp);
+    if (name == "ref" || name == "in" || name == "out" || name == "version" || name == "align") name ~= "_";
+
+    type = textNoName(pp);
+    needReturn = (type != "void");
+  }
+}
+
+
+// ////////////////////////////////////////////////////////////////////////// //
 void parseCommand (Element cmd) {
-  string getType (Element nr) {
-    auto tp = nr.querySelector("ptype");
-    if (tp is null) return "void";
-    auto res = tp.innerText.xstrip;
-    assert(res.length);
-    return res;
-  }
-
-  string getName (Element nr) {
-    auto ne = nr.querySelector("name");
-    assert(ne !is null);
-    auto res = ne.innerText.xstrip;
-    assert(res.length);
-    return res;
-  }
-
   assert(cmd !is null);
   auto nameres = cmd.querySelector("proto");
   assert(nameres !is null);
 
-  switch (getName(nameres)) {
+  auto fntype = TypeAndName(nameres);
+
+  switch (fntype.name) {
     case "glCreateSyncFromCLeventARB": return;
     case "glPathGlyphIndexRangeNV": return; // [2]
+    case "SwapBuffers": return; // shitdoze
+    case "ChoosePixelFormat": return; // shitdoze
+    case "DescribePixelFormat": return; // shitdoze
+    case "GetPixelFormat": return; // shitdoze
+    case "SetPixelFormat": return; // shitdoze
+    case "wglGetProcAddress": return; // shitdoze
+    case "glXGetProcAddress": return;
+    case "glXGetProcAddressARB": return;
     default: break;
   }
 
-  /*
-  write(getType(nameres), " ", getName(nameres), " (");
-  bool needComma = false;
-  foreach (Element param; cmd.querySelectorAll("param")) {
-    if (needComma) write(", "); else needComma = true;
-    write(getType(param), " ", getName(param));
-  }
-  writeln(");");
-  */
-
-  string textNoNameIntr (Element el) {
-    string s;
-    foreach (Element child; el.children) {
-      if (child.tagName == "name") continue;
-      if (child.nodeType != NodeType.Text) s ~= textNoNameIntr(child); else s ~= child.nodeValue();
-    }
-    return s;
+  if (fntype.name.endsWith("SGIX")) {
+    //stderr.writeln(fntype.name);
+    return;
   }
 
-  string fixPointers (string s) {
-    auto mt = s.matchFirst(xre);
-    if (mt.empty) return s;
-    string res = "const("~mt[1]~")"~mt[2]~" "~(mt[3].replaceAll(fuckre, "$1"));
-    return res;
+  TypeAndName[] args;
+  foreach (Element param; cmd.querySelectorAll("param")) args ~= TypeAndName(param);
+
+  if (fntype.name == "glXChooseVisual") {
+    args[2].type = "const(int)*";
   }
 
-  string textNoName (Element el) {
-    return fixPointers(textNoNameIntr(el)~" _")[0..$-2];
+  if (fntype.name == "glXGetProcAddress" || fntype.name == "glXGetProcAddressARB") {
+    args[0].type = "const(char)*";
   }
 
   string getArgs () {
     string res = "(";
     bool needComma = false;
-    foreach (Element param; cmd.querySelectorAll("param")) {
+    foreach (const ref TypeAndName tn; args) {
       if (needComma) res ~= ", "; else needComma = true;
-      res ~= fixPointers(param.innerText.xstrip.replaceAll(repre, "$1_"));
+      res ~= tn.type;
+      res ~= " ";
+      res ~= tn.name;
     }
     res ~= ")";
     return res;
@@ -83,26 +159,24 @@ void parseCommand (Element cmd) {
   string getCall () {
     string res = "(";
     bool needComma = false;
-    foreach (Element param; cmd.querySelectorAll("param")) {
+    foreach (const ref TypeAndName tn; args) {
       if (needComma) res ~= ", "; else needComma = true;
-      res ~= getName(param).replaceAll(repre, "$1_");
+      res ~= tn.name;
     }
     res ~= ")";
     return res;
   }
 
-  writeln("alias ", getName(nameres), " = ", getName(nameres), "_Z_Z;");
-  write("__gshared ", getName(nameres), "_Z_Z = function ", textNoName(nameres).xstrip, " ", getArgs(), " { ");
-  if (textNoName(nameres).xstrip != "void") write("return ");
-  write(getName(nameres), "_Z_Z_loader", getCall(), ";");
-  writeln(" };");
+  string intrName = fntype.name~"_Z_Z_";
+  string ldrName = intrName~"_loader_";
+
+  writeln("alias ", fntype.name, " = ", intrName, ";");
+  writeln("__gshared ", intrName, " = function ", fntype.type, " ", getArgs(), " { ", (fntype.needReturn ? "return " : ""), ldrName, getCall(), "; };");
   // loader
-  writeln("private ", textNoName(nameres).xstrip, " ", getName(nameres), "_Z_Z_loader ", getArgs(), " {");
-  writeln("  *cast(void**)&", getName(nameres), "_Z_Z = glbindGetProcAddress(`", getName(nameres), "`);");
-  writeln("  if (*cast(void**)&", getName(nameres), "_Z_Z is null) assert(0, `OpenGL function '", getName(nameres), "' not found!`);");
-  write("  ");
-  if (textNoName(nameres).xstrip != "void") write("return ");
-  writeln(getName(nameres), "_Z_Z", getCall(), ";");
+  writeln("private ", fntype.type, " ", ldrName, " ", getArgs(), " {");
+  writeln("  *cast(void**)&", intrName, " = glbindGetProcAddress(`", fntype.name, "`);");
+  writeln("  if (*cast(void**)&", intrName, " is null) assert(0, `OpenGL function '", fntype.name, "' not found!`);");
+  writeln("  ", (fntype.needReturn ? "return " : ""), intrName, getCall(), ";");
   writeln("}");
 
 /*
@@ -125,13 +199,20 @@ private const(char)* glbfn_glGetString_loader (uint a00) {
 }
 
 
-void main () {
-  gldom = new XmlDocument(readTextFile("gl.xml"));
+void main (string[] args) {
+  gldom = new XmlDocument(readTextFile(args.length == 1 ? "gl.xml" : args[1]));
   writeln("// enums");
   foreach (Element glenum; gldom.querySelectorAll("enums")) {
     //writeln(glenum.getAttribute("namespace"), ":", glenum.getAttribute("group"), " -- ", glenum.getAttribute("comment"));
     //<enum value="0x00000001" name="GL_CURRENT_BIT"/>
     foreach (Element it; glenum.querySelectorAll("enum[value][name]")) {
+      string name = it.getAttribute("name").xstrip;
+      assert(name.length);
+      if (name[0] == '_') continue;
+      if (name.endsWith("SGIX")) continue;
+      string value = it.getAttribute("value").xstrip;
+      assert(value.length);
+      if (value[0] == '"') continue;
       string api = it.getAttribute("api");
       if (api == "gles2") continue; // fuck off
       if (api.length && api != "gl") {
@@ -147,7 +228,7 @@ void main () {
       } else if (type.length) {
         assert(0, type);
       }
-      writeln("enum ", type, " ", it.getAttribute("name"), " = ", it.getAttribute("value"), pfx, ";");
+      writeln("enum ", type, " ", name, " = ", value, pfx, ";");
     }
   }
   writeln;
