321774f597438c028246d4241ecfbd4974813a79
Ketmar Dark <ketmar@ketmar.no-ip.org>
2018-02-08 00:34:29 +0200

    iv.nukedopl3: added simple DooM MUS / Midi player (and sample)

--- opl3test/musplay.d
+++ opl3test/musplay.d
@@ -9,12 +9,14 @@
 // GNU General Public License for more details.
 import iv.cmdcon;
 import iv.vfs;
+import iv.vfs.io;
 
 import iv.nukedopl3;
 import xalsa;
 
 
-__gshared OPL3Chip opl;
+////////////////////////////////////////////////////////////////////////////////
+__gshared OPLPlayer player;
 __gshared short[4096] smpbuf;
 __gshared uint smpbufpos;
 
@@ -33,32 +35,28 @@ void playbuf () {
 
 
 void main (string[] args) {
-  if (args.length < 2) assert(0, "file?");
-  auto fl = VFile(args[1]);
-  uint flen = fl.readNum!ushort;
-  if (flen == 0) {
-    flen = cast(uint)fl.size;
-    fl.seek(0);
-  }
-  opl.reset(48000);
-  alsaOpen(2);
-  scope(exit) alsaClose();
-  uint samplesLeft = 0;
-  mainloop: for (;;) {
-    while (samplesLeft == 0) {
-      if (flen < 4) break mainloop;
-      flen -= 4;
-      ubyte reg = fl.readNum!ubyte;
-      ubyte val = fl.readNum!ubyte;
-      samplesLeft = fl.readNum!ushort;
-      //conwritefln!"reg=0x%02x; val=0x%02x; delay=%s (%s)"(reg, val, samplesLeft, samplesLeft*48000/560);
-      opl.writeReg(reg, val);
-      if (samplesLeft) { samplesLeft = samplesLeft*48000/560; break; }
+  player = new OPLPlayer(48000, true);
+  version(genmidi_dumper) {
+    player.dumpGenMidi(stdout);
+  } else {
+    if (args.length < 2) assert(0, "file?");
+    auto fl = VFile(args[1]);
+    uint flen = cast(uint)fl.size;
+    assert(flen > 0);
+    alsaOpen(2);
+    scope(exit) alsaClose();
+
+    ubyte[] fdata = new ubyte[](flen);
+    fl.rawReadExact(fdata);
+    if (!player.load(fdata)) assert(0, "cannot load song");
+    delete fdata;
+    player.play();
+
+    while (player.playing) {
+      smpbufpos = player.generate(smpbuf[])*2;
+      if (smpbufpos == 0) break;
+      playbuf();
     }
-    --samplesLeft;
-    opl.generateStream(smpbuf[smpbufpos..smpbufpos+2]);
-    smpbufpos += 2;
-    if (smpbufpos >= smpbuf.length) playbuf();
+    playbuf();
   }
-  playbuf();
 }
