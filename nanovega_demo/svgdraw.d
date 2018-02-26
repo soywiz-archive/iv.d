fff41b8355d96a4b7473662acbde1a7e7049e18f
Ketmar Dark <ketmar@ketmar.no-ip.org>
2018-02-26 05:08:58 +0200

    svgdraw: cosmetix

--- nanovega_demo/svgdraw.d
+++ nanovega_demo/svgdraw.d
@@ -15,6 +15,8 @@
 //    misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 //
+module svgdraw;
+
 import core.stdc.stdio;
 import iv.nanovega;
 import iv.nanovega.svg;
